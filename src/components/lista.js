b55f793df43b4329a165db6576119888e73c6e45
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2020-05-19 20:47:54 -0300

    Se agregó la key"

--- src/components/lista.js
+++ src/components/lista.js
@@ -4,10 +4,11 @@ import "../css/lista.css";
 export default function lista(props) 
 {
     const {signoslista,handler}=props;
-    const signos=signoslista.map(e=>{
+    const signos=signoslista.map((e,i)=>{
        return <img onClick={handler} className="image" src={e.imagen} height="15%" width="15%" alt=""
        title={e.signo}
-       value={e.signo} />
+       value={e.signo}
+       key={i} />
       });
     return (
         <div className="image-container tooltip">{signos}</div>
